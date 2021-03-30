78dbe9986255155fae47c03d9957afc77c9255f3
JonatanMroczek <77212154+JonatanMroczek@users.noreply.github.com>
2021-03-30 22:56:14 +0200

    fix checkstyle

--- app/src/main/java/pl/futurecollars/invocing/App.java
+++ app/src/main/java/pl/futurecollars/invocing/App.java
@@ -1,9 +1,8 @@
 package pl.futurecollars.invocing;
 
 public class App {
-
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello World";
     }
 
     public static void main(String[] args) {
