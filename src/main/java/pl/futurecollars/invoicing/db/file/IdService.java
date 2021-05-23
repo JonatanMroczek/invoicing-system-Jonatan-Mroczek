8a062dc357883fb722f39ae616bf89345d17dd7d
JonatanMroczek <77212154+JonatanMroczek@users.noreply.github.com>
2021-05-23 18:12:53 +0200

    Delete unused import

--- src/main/java/pl/futurecollars/invoicing/db/file/IdService.java
+++ src/main/java/pl/futurecollars/invoicing/db/file/IdService.java
@@ -6,7 +6,6 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Repository;
 import pl.futurecollars.invoicing.utils.FilesService;
 
 @NoArgsConstructor
